6af04e8214b77592b091eabeebcd73a5b1acbd6f
zhuoyang <zhuoyang98@gmail.com>
2015-04-27 07:58:44 -0700

    Import from Sony Sources

---
+++ .missing-syscalls.d
@@ -0,0 +1,7 @@
+-:  /home/android/android/Sony/kernel/include/linux/kconfig.h \
+ include/generated/autoconf.h \
+ /home/android/android/Sony/kernel/arch/arm/include/asm/types.h \
+ include/asm-generic/int-ll64.h \
+ arch/arm/include/generated/asm/bitsperlong.h \
+ include/asm-generic/bitsperlong.h \
+ /home/android/android/Sony/kernel/arch/arm/include/asm/unistd.h
